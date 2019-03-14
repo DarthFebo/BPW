6ffc1737bbdd18ddd3ebe8fc7b8829354ee96895
Fvanbrummelen <darthfebo@users.noreply.github.com>
2019-03-14 12:01:37 +0100

    1st commit from MAC

---
+++ Zombie game/Assets/PlayerAim.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class PlayerAim : MonoBehaviour {
+
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+
+    [SerializeField] private Transform PlayerBody;
+
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+    }
+
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+
+    private void CameraRotation() 
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if(xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXaxisRotationToValue(270.0f);
+        }
+
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXaxisRotationToValue(90.0f);
+        }
+
+
+        transform.Rotate(Vector3.left * mouseY);
+        PlayerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXaxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+
+}
+
+//https://www.youtube.com/watch?v=n-KX8AeGK7E 
